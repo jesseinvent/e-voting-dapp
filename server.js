91b888c1984d39c2b44ebf7f9f421647408a3eb0
Jesse Ojih <jcinvent05@gmail.com>
2023-05-21 18:49:03 +0100

    Updates

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("src/index.html"));
